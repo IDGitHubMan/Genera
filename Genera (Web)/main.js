41e8c4e29e371681cea51585cebe7e0bfda511cf
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-01-23 11:33:38 -0500

    Basic files for web version.

---
+++ Genera (Web)/main.js
@@ -0,0 +1,13 @@
+class Node{}
+
+class Drifter{}
+
+class DrifterCol{}
+
+const warp = new p5((sketch) => { });
+
+const flow = new p5((sketch) => { });
+
+const paths = new p5((sketch) => { });
+
+const cells = new p5((sketch) => { });
