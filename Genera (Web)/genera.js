815861680c2ee538148efb0d11ca6883cb527178
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-01-25 11:10:57 -0500

    Remove instance mode, since there'll only be one sketch.

--- Genera (Web)/genera.js
+++ Genera (Web)/genera.js
@@ -1,16 +1,16 @@
 class Node {
     constructor(parent) {
         this.p = parent;
-        this.loc = this.p.s.createVector(this.p.s.random(this.p.s.width), this.p.s.random(this.p.s.height))
-        this.vel = this.p.s.createVector(this.p.s.random(-1, 1), this.p.s.random(-1, 1));
-        this.color = this.p.s.color(this.p.s.random(255), this.p.s.random(255), this.p.s.random(255));
-        this.range = this.p.s.random(0, 1);
-        this.connectLimit = Math.floor(this.p.s.random(0, 5));
+        this.loc = createVector(random(width), random(height))
+        this.vel = createVector(random(-1, 1), random(-1, 1));
+        this.color = color(random(255), random(255), random(255));
+        this.range = random(0, 1);
+        this.connectLimit = Math.floor(random(0, 5));
         this.nearest = [];
     }
 
     checkNearest(nodes){
-        let distanceArray = nodes.map(node => this.p.s.distance(this.loc.x,node.loc.x,this.loc.y,node.loc.y));
+        let distanceArray = nodes.map(node => distance(this.loc.x,node.loc.x,this.loc.y,node.loc.y));
         for (let i = 0; i < this.p.connectLimit; i++){
             this.nearest.push(nodes[distanceArray.indexOf(Math.min(distanceArray))]);
         }
@@ -19,30 +19,30 @@ class Node {
     update() {
         this.checkNearest(this.p.nodes);
         this.vel.normalize();
-        let actRange = this.p.s.map(range, 0, 1, this.p.minRange, this.p.maxRange);
-        let drift = this.p.s.createVector(this.p.s.map(this.vel.x, -1, 1, -this.p.driftLimit, this.p.driftLimit), map(vel.y, -1, 1, -this.p.driftLimit, this.p.driftLimit));
-        this.loc.x = constrain(loc.x + drift.x, 0, this.p.s.width);
-        this.loc.y = constrain(loc.y + drift.y, 0, this.p.s.height);
+        let actRange = map(range, 0, 1, this.p.minRange, this.p.maxRange);
+        let drift = createVector(map(this.vel.x, -1, 1, -this.p.driftLimit, this.p.driftLimit), map(vel.y, -1, 1, -this.p.driftLimit, this.p.driftLimit));
+        this.loc.x = constrain(loc.x + drift.x, 0, width);
+        this.loc.y = constrain(loc.y + drift.y, 0, height);
         if (this.p.loopingEdges) {
-            if (this.loc.x == 0 || this.loc.x == this.p.s.width) {
+            if (this.loc.x == 0 || this.loc.x == width) {
                 this.vel.x = -this.vel.x;
             }
 
-            if (this.loc.y == 0 || this.loc.y == this.p.s.height) {
+            if (this.loc.y == 0 || this.loc.y == height) {
                 this.vel.y = -this.vel.y;
             }
         } else {
             if (this.loc.x == 0) {
-                this.loc.x = this.p.s.width - 1;
+                this.loc.x = width - 1;
             }
-            if (this.loc.x == this.p.s.width) {
+            if (this.loc.x == width) {
                 this.loc.x = 1;
             }
 
             if (this.loc.y == 0) {
-                this.loc.y = this.p.s.height - 1;
+                this.loc.y = height - 1;
             }
-            if (this.loc.y == this.p.s.height) {
+            if (this.loc.y == height) {
                 this.loc.y = 1;
             }
         }
@@ -87,18 +87,6 @@ class Path { }
 
 class PathManager { }
 
-const nodes = new p5((sketch) => {
-    sketch.setup = () => {
-        let c = sketch.createCanvas(400, 400);
-        c.parent("graphCanvas");
-        sketch.background(0);
-    }
-});
+function setup(){}
 
-const warp = new p5((sketch) => { });
-
-const flow = new p5((sketch) => { });
-
-const paths = new p5((sketch) => { });
-
-const cells = new p5((sketch) => { });
+function draw(){}
