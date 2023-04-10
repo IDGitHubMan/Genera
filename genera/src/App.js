2bd5e377a9efdcc588d0dfdb7a72c59fe87d98bc
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-04-10 12:01:00 -0400

    Add home to routes.

--- genera/src/App.js
+++ genera/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import NotFound from "./Components/NotFound";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <main>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
