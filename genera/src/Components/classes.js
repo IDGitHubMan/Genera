1b2fd9a18ef53a7d5c5085b3adce0c1d64707c85
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-04-10 12:02:23 -0400

    Add more classes for gens.

--- genera/src/Components/classes.js
+++ genera/src/Components/classes.js
@@ -88,3 +88,409 @@ export class Star {
     }
   }
 }
+
+export class Storm {
+  constructor(s) {
+    this.sketch = s;
+    this.strikeChance = 0.005;
+    class Drop {
+      constructor(s) {
+        this.sketch = s;
+        this.mass = this.sketch.random(0.1, 2.0);
+        this.gravity = this.sketch.createVector(0, 9.8 * this.mass * 4);
+        this.splashes = this.sketch.int(this.sketch.random(1, 20));
+        this.l = this.sketch.map(this.mass, 0.1, 2.0, 25, 75);
+        this.loc = this.sketch.createVector(
+          this.sketch.random(this.sketch.width),
+          this.sketch.random(this.sketch.height)
+        );
+        this.col = this.sketch.color(255, 50);
+      }
+
+      update() {
+        if (this.loc.y >= this.sketch.height) {
+          this.loc = this.sketch.createVector(
+            this.sketch.random(this.sketch.width),
+            0
+          );
+          this.mass = this.sketch.random(0.1, 2.0);
+          this.mass = this.sketch.random(0.1, 2.0);
+          this.gravity = this.sketch.createVector(0, 9.8 * this.mass * 4);
+          this.splashes = this.sketch.int(this.sketch.random(1, 20));
+        } else {
+          this.loc.add(this.gravity);
+        }
+        this.sketch.stroke(this.col);
+        this.sketch.strokeWeight(1);
+        this.sketch.line(
+          this.loc.x,
+          this.loc.y,
+          this.loc.x,
+          this.loc.y - this.l
+        );
+      }
+    }
+    this.droplets = [];
+    for (let i = 0; i < 200; i++) {
+      this.droplets[i] = new Drop(this.sketch);
+    }
+    this.lightnings = [];
+  }
+
+  update() {
+    this.sketch.background(0, 0, 66);
+    class Point {
+      constructor(s, x, y) {
+        this.sketch = s;
+        this.pos = this.sketch.createVector(x, y);
+      }
+
+      show() {
+        this.sketch.point(this.pos.x, this.pos.y);
+      }
+    }
+    class Lightning {
+      constructor(s) {
+        this.sketch = s;
+        this.stayCounter = 0;
+        this.points = [];
+        this.splitChance = 0;
+        class Header {
+          constructor(s) {
+            this.sketch = s;
+            this.loc = this.sketch.createVector(
+              this.sketch.random(10, this.sketch.width - 10),
+              0
+            );
+            this.end = this.sketch.createVector(
+              this.sketch.random(this.sketch.width),
+              this.sketch.height
+            );
+          }
+
+          insertPoints() {
+            this.split = this.sketch.random(1);
+            let behaviorChoice = this.sketch.int(this.sketch.random(3));
+            if (behaviorChoice == 0) {
+              if (this.loc.x > this.end.x) {
+                this.loc.x -= 1;
+              } else if (this.loc.x < this.end.x) {
+                this.loc.x += 1;
+              }
+              this.loc.y += 1;
+            } else {
+              let direct = this.sketch.createVector(
+                this.sketch.random(-5, 5),
+                this.sketch.random(-2, 5)
+              );
+              this.loc.add(direct);
+            }
+            return this.loc;
+          }
+        }
+        var h1 = new Header(this.sketch);
+        this.headers = [h1];
+        this.points = [];
+      }
+
+      update() {
+        for (var head of this.headers) {
+          var res = head.insertPoints();
+          if (
+            res.x > this.sketch.width ||
+            res.x < 0 ||
+            res.y > this.sketch.height
+          ) {
+            this.stayCounter += 1;
+            let a = this.sketch.map(this.stayCounter, 0, 25, 255, 0);
+            let w = this.sketch.map(this.stayCounter, 0, 25, 10, 0);
+            for (var p of this.points) {
+              this.sketch.stroke(255, a);
+              this.sketch.strokeWeight(w);
+              p.show();
+            }
+          } else {
+            if (head.split <= this.splitChance) {
+              this.headers.push(head);
+            }
+            this.points.push(new Point(this.sketch, res.x, res.y));
+          }
+        }
+      }
+    }
+    var strike = this.sketch.random(1);
+    if (strike <= this.strikeChance) {
+      this.lightnings.push(new Lightning(this.sketch));
+    }
+    for (var d of this.droplets) {
+      d.update();
+    }
+    for (var lightning of this.lightnings) {
+      if (lightning.stayCounter > 25) {
+        this.lightnings.splice(
+          this.lightnings.indexOf(lightning),
+          this.lightnings.indexOf(lightning) + 1
+        );
+      }
+      lightning.update();
+    }
+  }
+}
+
+export class Node {
+  constructor(s) {
+    this.sketch = s;
+    this.drift = this.sketch.createVector(
+      this.sketch.random(-2, 2),
+      this.sketch.random(-2, 2)
+    );
+    this.col = this.sketch.color(
+      this.sketch.random(255),
+      this.sketch.random(255),
+      this.sketch.random(255)
+    );
+    this.range = this.sketch.random(20, 200);
+    this.loc = this.sketch.createVector(
+      this.sketch.random(this.sketch.width),
+      this.sketch.random(this.sketch.height)
+    );
+  }
+  update() {
+    this.loc.add(this.drift);
+    if (this.loc.x > this.sketch.width || this.loc.x < 0) {
+      this.drift.x *= -1;
+    }
+    if (this.loc.y > this.sketch.height || this.loc.y < 0) {
+      this.drift.y *= -1;
+    }
+    this.sketch.fill(this.col);
+    this.sketch.noStroke();
+    this.sketch.ellipse(this.loc.x, this.loc.y, 1, 1);
+  }
+}
+
+export class Graph {
+  constructor(s) {
+    this.sketch = s;
+    this.nodeStore = [];
+    for (let i = 0; i < 75; i++) {
+      let n = new Node(this.sketch);
+      this.nodeStore[i] = n;
+    }
+  }
+  update() {
+    this.sketch.background(0, 0, 0, 40);
+    for (var node of this.nodeStore) {
+      node.update();
+      for (var node2 of this.nodeStore) {
+        let distance = this.sketch.dist(
+          node.loc.x,
+          node.loc.y,
+          node2.loc.x,
+          node2.loc.y
+        );
+        let a = this.sketch.map(distance, 0, node.range, 255, 0);
+        if (distance == 0) {
+          this.sketch.noStroke();
+        } else if (distance <= node.range) {
+          this.sketch.stroke(
+            this.sketch.red(node.col),
+            this.sketch.green(node.col),
+            this.sketch.blue(node.col),
+            a
+          );
+          if (distance <= node2.range) {
+            this.sketch.line(
+              node.loc.x,
+              node.loc.y,
+              (node.loc.x + node2.loc.x) / 2,
+              (node.loc.y + node2.loc.y) / 2
+            );
+          } else {
+            this.sketch.line(node.loc.x, node.loc.y, node2.loc.x, node2.loc.y);
+          }
+        }
+      }
+    }
+  }
+}
+
+export class Flow {
+  constructor(s) {
+    this.sketch = s;
+    this.loc = this.sketch.createVector(
+      this.sketch.random(this.sketch.width),
+      this.sketch.random(this.sketch.height)
+    );
+    this.acc = this.sketch.createVector(0, 0);
+    this.vel = this.sketch.createVector(0, 0);
+    this.xInc = 0.01;
+    this.yInc = 10;
+    this.resolution = 1;
+  }
+  update() {
+    var theta = this.sketch.map(
+      this.sketch.noise(
+        this.sketch.floor(this.loc.x / this.resolution) * this.xInc,
+        this.sketch.floor(this.loc.y / this.resolution) * this.yInc,
+        this.sketch.millis() / 10000
+      ),
+      0,
+      1,
+      -this.sketch.TWO_PI,
+      this.sketch.TWO_PI
+    );
+    var flowForce = this.sketch.createVector(
+      this.sketch.cos(theta),
+      this.sketch.sin(theta)
+    );
+    flowForce.mult(100);
+    this.acc.add(flowForce);
+    this.vel.add(this.acc);
+    this.vel.limit(5);
+    this.loc.add(this.vel);
+    if (this.loc.x > this.sketch.width) {
+      this.loc.x = 0;
+    }
+    if (this.loc.x < 0) {
+      this.loc.x = this.sketch.width;
+    }
+    if (this.loc.y > this.sketch.height) {
+      this.loc.y = 0;
+    }
+    if (this.loc.y < 0) {
+      this.loc.y = this.sketch.height;
+    }
+    this.acc.mult(0);
+    this.sketch.strokeWeight(1);
+    this.sketch.stroke(255);
+    this.sketch.point(this.loc.x, this.loc.y);
+  }
+}
+
+export class FlowSet {
+  constructor(s) {
+    this.sketch = s;
+    this.xInc = this.sketch.random(0.5);
+    this.yInc = this.sketch.random(0.5);
+    this.resolution = this.sketch.random(5);
+    this.flows = new Array(2500);
+    for (let i = 0; i < this.flows.length; i++) {
+      this.flows[i] = new Flow(this.sketch);
+      this.flows[i].xInc = this.xInc;
+      this.flows[i].yInc = this.yInc;
+      this.flows[i].resolution = this.resolution;
+    }
+  }
+  update() {
+    this.sketch.background(0, 40);
+    for (var flow of this.flows) {
+      flow.update();
+    }
+  }
+}
+
+export class RandGraphs {
+  constructor(s) {
+    this.sketch = s;
+    this.randomCounts = [];
+    this.gaussianCounts = [];
+    this.monteCarloCounts = [];
+    this.invMonteCarloCounts = [];
+
+    for (let i = 0; i < this.sketch.width; i++) {
+      this.randomCounts[i] = 0;
+      this.gaussianCounts[i] = 0;
+      this.monteCarloCounts[i] = 0;
+      this.invMonteCarloCounts[i] = 0;
+    }
+  }
+
+  monteCarlo() {
+    while (true) {
+      let r1 = this.sketch.random(1);
+      let r2 = this.sketch.random(1);
+      if (r2 >= r1) {
+        return r1;
+      }
+    }
+  }
+
+  invMonteCarlo() {
+    while (true) {
+      let r1 = this.sketch.random(1);
+      let r2 = this.sketch.random(1);
+      if (r2 <= r1) {
+        return r1;
+      }
+    }
+  }
+
+  update() {
+    this.sketch.background(0, 0, 66);
+    let rand = this.sketch.int(this.sketch.random(this.randomCounts.length));
+    let gauss = this.sketch.int(
+      this.sketch.constrain(
+        (this.sketch.randomGaussian() * this.sketch.width) / 8 +
+          this.sketch.width / 2,
+        0,
+        this.sketch.width
+      )
+    );
+    let monte = this.sketch.int(
+      this.monteCarlo() * this.monteCarloCounts.length
+    );
+    let iMonte = this.sketch.int(
+      this.invMonteCarlo() * this.invMonteCarloCounts.length
+    );
+
+    this.randomCounts[rand] += 1;
+    this.gaussianCounts[gauss] += 1;
+    this.monteCarloCounts[monte] += 1;
+    this.invMonteCarloCounts[iMonte] += 1;
+
+    for (let x = 0; x < this.randomCounts.length; x++) {
+      this.sketch.stroke(0, 255, 0, 64);
+      this.sketch.line(
+        x,
+        this.sketch.height,
+        x,
+        this.sketch.height - this.gaussianCounts[x] * 10
+      );
+      this.sketch.stroke(255, 0, 0, 64);
+      this.sketch.line(
+        x,
+        this.sketch.height,
+        x,
+        this.sketch.height - this.monteCarloCounts[x] * 10
+      );
+      this.sketch.stroke(0, 0, 255, 64);
+      this.sketch.line(
+        x,
+        this.sketch.height,
+        x,
+        this.sketch.height - this.invMonteCarloCounts[x] * 10
+      );
+      this.sketch.stroke(0, 0, 0, 64);
+      this.sketch.line(
+        x,
+        this.sketch.height,
+        x,
+        this.sketch.height - this.randomCounts[x] * 10
+      );
+
+      if (this.gaussianCounts[x] * 10 > this.sketch.height) {
+        this.gaussianCounts[x] = 0;
+      }
+      if (this.monteCarloCounts[x] * 10 > this.sketch.height) {
+        this.monteCarloCounts[x] = 0;
+      }
+      if (this.invMonteCarloCounts[x] * 10 > this.sketch.height) {
+        this.invMonteCarloCounts[x] = 0;
+      }
+      if (this.randomCounts[x] * 10 > this.sketch.height) {
+        this.randomCounts[x] = 0;
+      }
+    }
+  }
+}
