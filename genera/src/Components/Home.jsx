54feb5e0b9c6345833cd1312766764e0fbca7f70
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-04-13 10:18:44 -0400

    Update Home.jsx

--- genera/src/Components/Home.jsx
+++ genera/src/Components/Home.jsx
@@ -1,13 +1,17 @@
 import HomeAnim from "./HomeAnim";
+import Nav from "./Nav";
 export const Home = () => {
   return (
-    <div className="align-center align-middle">
-      <div className="align-center text-center text-white z-10 relative">
-        <h1 className="text-7xl">Welcome to Genera</h1>
-        <p className="text-xl">
-          Genera has multiple "systems/gens" that you can use to create abstract
-          and vibrant visuals.
-        </p>
+    <div>
+      <Nav />
+      <div className="flex align-center text-left text-white z-10 relative w-1/2 h-10">
+        <div>
+          <h1 className="text-5xl">Welcome to Genera</h1>
+          <p className="text-xl">
+            Genera has multiple "systems/gens" that you can use to create
+            abstract and vibrant visuals.
+          </p>
+        </div>
       </div>
       <HomeAnim />
     </div>
